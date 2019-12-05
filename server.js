8a89a06da1ede7140d4e0250fc5989aecf17ce41
ngaivoronski <ngaivoronski@gmail.com>
2019-12-05 10:16:42 -0500

    mvp

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +17,13 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const timestamp = new Date();
+  console.log(`${req.method} to ${req.originalUrl} at ${timestamp}`);
+  next();
+}
+
+server.use('/api/posts', helmet(), postRouter);
+server.use('/api/users',helmet(), userRouter);
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,46 +2,118 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
-});
-
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.get('/', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
-  // do your magic!
-});
+const userDb = require('./userDb');
+const postDb = require('../posts/postDb');
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  userDb.getById(req.params.id)
+    .then(user => {
+      if(user && user.id) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({  message: "Invalid user id." });
+      }
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (req.body && req.body.name) {
+      next();
+  } else if (req.body) {
+      res.status(400).json({ message: "Missing required name field." });
+  } else {
+    res.status(400).json({ message: "Missing user data." })
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(req.body && req.body.text) {
+    next();
+  } else if (req.body) {
+      res.status(400).json({ message: "Missing required text field." });
+  } else {
+    res.status(400).json({ message: "Missing post data." })
+  }
 }
+
+// CRUD
+
+router.get('/', (req, res, next) => {
+  userDb.get()
+    .then( users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "Users could not be retrieved." });
+    })
+});
+
+router.get('/:id', validateUserId, (req, res, next) => {
+  res.status(200).json(req.user);
+});
+
+router.get('/:id/posts', validateUserId, (req, res) => {
+  userDb.getUserPosts(req.params.id)
+    .then(data=>{
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error getting the user's posts." });
+    });
+});
+
+router.post('/', (req, res, next) => {
+  
+  userDb.insert(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error adding the user." });
+    });
+});
+
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const newPost = {...req.body, user_id: req.user.id}
+
+  postDb.insert(newPost)
+    .then(postData=>{
+      res.status(201).json(postData);
+    })
+    .catch(err=>{
+      res.status(500).json({ error: "There was an error posting the post." });
+    });
+});
+
+router.delete('/:id', validateUserId, (req, res) => {
+  userDb.remove(req.user.id)
+    .then(confirmation => {
+      if (confirmation > 0) {
+        res.status(201).json({ message: "User was successfully deleted." });
+      } else {
+        res.status(500).json({ error: "There was an error deleting the user." });
+      }
+    })
+    .catch(err=>{
+      res.status(500).json({ error: "There was an error deleting the user." });
+    });
+});
+
+router.put('/:id', validateUserId, validateUser, (req, res) => {
+  userDb.update(req.user.id, req.body)
+    .then(confirmation => {
+      if (confirmation > 0) {
+        res.status(201).json({ message: "User was successfully updated." });
+      } else {
+        res.status(500).json({ error: "There was an error updating deleting the user." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: "There was an error updating deleting the user." });
+    });
+});
 
 module.exports = router;
